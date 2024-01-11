d7b917b3c33fb6ef99b2f0465a3f94a8d8a5a17e
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-11 09:15:11 -0800

    0-run

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -1,24 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>  // Include any necessary standard libraries
+#include <stdio.h>
 
-/**
- * struct Node - A structure representing a node in a linked list
- * @data: The data stored in the node
- * @next: Pointer to the next node in the list
- */
-typedef struct Node
-{
+/* Structure definition for a linked list node */
+typedef struct Node {
     int data;
-    struct Node *next;
+    struct Node* next;
 } Node;
 
 /* Function prototypes */
-void print_list(const Node *head);
-int pop_list(Node **head);
-Node *add_node(Node **head, int data);
-void free_list(Node *head);
+Node* createNode(int data);
+void freeList(Node* head);
+void printList(Node* head);
 
 #endif /* LISTS_H */
 
