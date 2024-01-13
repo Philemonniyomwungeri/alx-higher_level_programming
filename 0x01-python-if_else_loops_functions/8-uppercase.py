7f3f02f802e992929e417bd8547936620e43b5df
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-13 11:29:08 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
 def uppercase(s):
     result = ""
     for char in s:
-        result += "{:c}".format(ord(char - 32) if 'a' <= char <= 'z' else ord(char))
+        result += "{:c}".format(ord(char) - 32) if 'a' <= char <= 'z' else "{:c}".format(ord(char))
     print(result)
 
 if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
+    uppercase("holberton")
+    uppercase("Holberton School")
+    uppercase("Holberton School, 98 battery street")
+    uppercase("")
+    uppercase("98")
+    uppercase("z")
 
