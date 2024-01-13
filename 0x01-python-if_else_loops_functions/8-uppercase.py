3cf48aeafbef8588cda8f94b7358514c5986e64e
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-13 11:24:53 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ""
     for char in s:
-        print("{:c}".format(ord(char - 32) if 'a' <= char <= 'z' else ord(char)), end="")
-    print()
+        result += "{:c}".format(ord(char - 32) if 'a' <= char <= 'z' else ord(char))
+    print(result)
 
 if __name__ == "__main__":
-    uppercase = __import__('8-uppercase').uppercase
-
     uppercase("best")
     uppercase("Best School 98 Battery street")
 
