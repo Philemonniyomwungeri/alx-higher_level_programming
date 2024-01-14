b4305e2ebef30f765dbe3983ee3fd8482798cc1e
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-14 15:43:18 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+
+def add(a, b):
+    """Return the sum of two integers."""
+    return a + b
+
+def sub(a, b):
+    """Return the result of subtracting b from a."""
+    return a - b
+
+def mul(a, b):
+    """Return the product of two integers."""
+    return a * b
+
+def div(a, b):
+    """Return the result of dividing a by b as an integer."""
+    return int(a / b)
 
 if __name__ == "__main__":
-    a = 10
-    b = 5
+    # Example usage:
+    x = 10
+    y = 5
 
-    add_result = add(a, b)
-    sub_result = sub(a, b)
-    mul_result = mul(a, b)
-    div_result = div(a, b)
+    result_add = add(x, y)
+    result_sub = sub(x, y)
+    result_mul = mul(x, y)
+    result_div = div(x, y)
 
-    print(f"{a} + {b} = {add_result}")
-    print(f"{a} - {b} = {sub_result}")
-    print(f"{a} * {b} = {mul_result}")
-    print(f"{a} / {b} = {div_result}")
+    print(f"{x} + {y} = {result_add}")
+    print(f"{x} - {y} = {result_sub}")
+    print(f"{x} * {y} = {result_mul}")
+    print(f"{x} / {y} = {result_div}")
+
