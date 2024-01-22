9037d56c9acf080c41b5d3d0cd826d9553f0b29c
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-22 13:25:59 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
+
+# Uncomment the following lines to test the function
+# value = 89
+# has_been_print = safe_print_integer_err(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
+#
+# value = -89
+# has_been_print = safe_print_integer_err(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
+#
+# value = "School"
+# has_been_print = safe_print_integer_err(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
