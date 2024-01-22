44f202589b1412379da482070cd371a6972ea88c
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-22 11:41:13 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -20,4 +20,3 @@ def safe_print_list(my_list=[], x=0):
 # print("nb_print: {:d}".format(nb_print))
 # nb_print = safe_print_list(my_list, len(my_list) + 2)
 # print("nb_print: {:d}".format(nb_print))
-
