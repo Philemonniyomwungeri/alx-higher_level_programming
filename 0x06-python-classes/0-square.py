ed8b2087f2a8189adf4de11b6e85e8a9f27655c8
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-23 10:44:30 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+
+This module defines the Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+    - No attributes yet.
+    """
+    pass
