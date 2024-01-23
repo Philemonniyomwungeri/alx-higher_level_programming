9fbe88687abd334360cd166c43054c3be28a7ac4
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-23 12:39:19 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Module 4-square
+
+This module defines the Square class with private size attribute, size validation,
+a public method to calculate the area, and getter and setter for size.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+    - __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+        - size (int): The size of the square (default is 0).
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+        - The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+        - The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Parameters:
+        - value: The size to set.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
