a047164ac5e0fd68bf94057c0446298060af11ef
Philemon Niyomwungeri <papy37788@gmail.com>
2024-01-23 14:29:50 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,25 +7,25 @@ class Node:
 
     @property
     def data(self):
-        """data getter"""
+        """Data getter"""
         return self.__data
 
     @data.setter
     def data(self, value):
-        """data setter"""
-        if type(value) != int:
+        """Data setter"""
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """next_node getter"""
+        """Next node getter"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """next_node setter"""
-        if value is not None and type(value) is not Node:
+        """Next node setter"""
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
@@ -36,26 +36,25 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
-        new = Node(value)
+        new_node = Node(value)
         tmp = self.__head
         add_start = False
 
         if not self.__head:
-            self.__head = new
-            new.next_node = None
+            self.__head = new_node
+            new_node.next_node = None
         else:
             if value < self.__head.data:
                 add_start = True
-            while tmp.next_node and value > tmp.next_node.data\
-                    and not add_start:
+            while tmp.next_node and value > tmp.next_node.data and not add_start:
                 tmp = tmp.next_node
             if not add_start:
-                    new.next_node = tmp.next_node
-                    tmp.next_node = new
+                new_node.next_node = tmp.next_node
+                tmp.next_node = new_node
             else:
-                new.next_node = tmp
-                self.__head = new
-            new.data = value
+                new_node.next_node = tmp
+                self.__head = new_node
+            new_node.data = value
 
     def __str__(self):
         s = ""
@@ -64,4 +63,20 @@ class SinglyLinkedList:
         while current:
             s += str(current.data) + '\n'
             current = current.next_node
-        return s[: -1]
+        return s[:-1]
+
+# Test the SinglyLinkedList class with examples
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    sll.sorted_insert(2)
+    sll.sorted_insert(5)
+    sll.sorted_insert(3)
+    sll.sorted_insert(10)
+    sll.sorted_insert(1)
+    sll.sorted_insert(-4)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(5)
+    sll.sorted_insert(12)
+    sll.sorted_insert(3)
+    print(sll)
