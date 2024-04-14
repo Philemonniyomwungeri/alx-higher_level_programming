9cf72ef263238fc4b7689ebc6cfd395bda6714ac
Philemonniyomwungeri <papy37788@gmail.com>
2024-04-14 03:53:25 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-import sys
+"""Lists states"""
+
 import MySQLdb
-
-if __name__ == "__main__":
-    # Connect to MySQL server
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute the query to select states starting with "N"
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows and display them
-    for row in cur.fetchall():
-        print(row)
-
-    # Close cursor and connection
-    cur.close()
-    conn.close()
+from sys import argv
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
