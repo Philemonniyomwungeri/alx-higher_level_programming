8eb6435ac3560f4576e5babfe0a0339c42150c34
Philemonniyomwungeri <papy37788@gmail.com>
2024-05-27 11:23:39 -0700

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Check if URL argument is provided
+// if (process.argv.length !== 3) {
+//   console.error('Usage: ./2-statuscode.js <URL>');
+//     process.exit(1);
+//     }
+//
+//     const url = process.argv[2];
+//
+//     // Make a GET request to the provided URL
+//     request.get(url, (error, response) => {
+//       if (error) {
+//           console.error(error);
+//               process.exit(1);
+//                 }
+//                   
+//                     console.log(`code: ${response.statusCode}`);
+//                     });
