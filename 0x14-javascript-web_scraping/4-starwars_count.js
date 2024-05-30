e3d231cddf02d8aab1aa035768c05aa31ffffa4b
Philemonniyomwungeri <papy37788@gmail.com>
2024-05-30 07:37:30 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const wedgeId = 18;
+
+request(apiUrl, function (error, response, body){
+	if (error){
+		console.error('error:', error);
+	}
+	else if (response.statusCode !== 200) {
+		    console.error(`Error: ${response.statusCode}`);
+	} else {
+		    const films = JSON.parse(body).results;
+		    let count = 0;
+
+		    films.forEach(film => {
+			          if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)) {
+					  count++;}
+		    });
+
+		console.log(count);
+	}
+});
