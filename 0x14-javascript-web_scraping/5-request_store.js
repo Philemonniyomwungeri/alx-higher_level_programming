6896d2ddfd862b9b3a93bbb2e714b9d772c2f956
Philemonniyomwungeri <papy37788@gmail.com>
2024-05-30 08:08:37 -0700

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (error, response, body) {
+	if (error) {
+		console.error('error:', error);
+		else if (response.statusCode !== 200) {
+			console.error(`Error: ${response.statusCode}`);
+		} else {
+			fs.writeFile(filePath, body, 'utf8', function (err) {
+				if (err) {
+					console.error('Error writing to file:', err);
+				}
+			});
+		}
+	});
