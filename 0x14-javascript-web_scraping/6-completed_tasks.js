97c551fe61813ad29a7d03a552160009dffdba9b
Philemonniyomwungeri <papy37788@gmail.com>
2024-05-30 09:14:17 -0700

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+	if (error) {
+		console.error('error:', error);
+	} else if (response.statusCode !== 200) {
+		console.error(`Error: ${response.statusCode}`);
+	} else {
+		const tasks = JSON.parse(body);
+		const completedTasks = {};
+		tasks.forEach(task => {
+			if (task.completed) {
+				if (!completedTasks[task.userId]) {
+					completedTasks[task.userId] = 0;
+					completedTasks[task.userId]++;
+				}
+			});
+		console.log(completedTasks);
+	}
+	});
